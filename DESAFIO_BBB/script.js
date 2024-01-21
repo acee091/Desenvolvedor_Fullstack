d73bb80a5c2e988fc643d54fe37338804619fd8e
acee091 <aceevangelista091@gmail.com>
2024-01-21 20:54:16 -0300

    modificações

---
+++ DESAFIO_BBB/script.js
@@ -0,0 +1,6 @@
+const btnredes = document.querySelector('.btn-redes');
+const redes = document.querySelector('.redes-sociais');
+
+btnredes.addEventListener('click', () =>{
+    redes.classList.add('.active');
+})
